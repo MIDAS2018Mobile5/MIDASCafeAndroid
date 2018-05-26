400f12965bf43d67069ee419452728675ddda7c0
eunjin Jo <eunjin3786@naver.com>
2018-05-27 06:56:57 +0900

    commmit

--- app/src/main/java/com/midas2018mobile5/mobileapp/main/activities/MenuAddActivity.java
+++ app/src/main/java/com/midas2018mobile5/mobileapp/main/activities/MenuAddActivity.java
@@ -1,19 +1,37 @@
 package com.midas2018mobile5.mobileapp.main.activities;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.midas2018mobile5.mobileapp.R;
 
-public class MenuAddActivity extends AppCompatActivity {
+public class MenuAddActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_add);
+
+        ImageButton imageButton_back = (ImageButton)findViewById(R.id.imageButton_back);
+        imageButton_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),AdminActivity.class);
+                //intent.putExtra("",);
+                startActivity(intent);
+            }
+        });
+
+
+
+
 
         ImageButton imageButton = (ImageButton)findViewById(R.id.imageButton_back);
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +42,22 @@ public class MenuAddActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        imageView = findViewById(R.id.imageView_menu_photo_add);
+        imageView.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view){
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        imageView.setImageURI(data.getData());
     }
 }
--- app/src/main/java/com/midas2018mobile5/mobileapp/main/activities/MenuEditActivity.java
+++ app/src/main/java/com/midas2018mobile5/mobileapp/main/activities/MenuEditActivity.java
@@ -1,17 +1,22 @@
 package com.midas2018mobile5.mobileapp.main.activities;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.midas2018mobile5.mobileapp.R;
 
-public class MenuEditActivity extends AppCompatActivity {
+public class MenuEditActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    ImageView imageView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_edit);
 
@@ -24,5 +29,20 @@ public class MenuEditActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        imageView = findViewById(R.id.imageView_menu_photo_add);
+        imageView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        imageView.setImageURI(data.getData());
     }
 }
